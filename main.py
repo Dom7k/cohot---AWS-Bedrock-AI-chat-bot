93e74f8ac9bb8673e562670338ed6e521b427314
dom7k <dominicnaatey@gmail.com>
2024-05-28 20:11:04 +0000

    Define Chatbot Function

--- main.py
+++ main.py
@@ -22,3 +22,16 @@ llm = Bedrock(
     client=bedrock_client,
     model_kwargs={"max_tokens_to_sample": 2000,"temperature":0.9}
 )
+
+def my_chatbot(language,freeform_text):
+    prompt = PromptTemplate(
+        input_variables=["language", "freeform_text"],
+        template="You are a chatbot. You are in {language}.\n\n{freeform_text}"
+    )
+
+    bedrock_chain = LLMChain(llm=llm, prompt=prompt)
+
+    response=bedrock_chain({'language':language, 'freeform_text':freeform_text})
+    return response
+
+# print(my_chatbot("english","which country is most advanced in technology?"))
